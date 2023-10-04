28470acdd0afdd78555e0d5ffc2b176a6dad704e
Lucas Madranges <lucas.madranges.pro@icloud.com>
2023-10-04 17:57:31 +0200

    Avancement du projet

--- script.js
+++ script.js
@@ -31,6 +31,45 @@ arrayBtn.forEach((btn) => {
 
 const scorePlayer1El = document.querySelector("#score--0");
 const scorePlayer2El = document.querySelector("#score--1");
+const currentScorePlayer1El = document.querySelector("#current--0");
+const currentScorePlayer2El = document.querySelector("#current--1");
+
+const sectionPlayer1 = document.querySelector(".player--0");
+const sectionPlayer2 = document.querySelector(".player--1");
+
+const diceEl = document.querySelector(".dice");
+
+const btnNew = document.querySelector(".btn--new");
+const btnHold = document.querySelector(".btn--hold");
+const btnRoll = document.querySelector(".btn--roll");
+
+let arrayScore = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 scorePlayer1El.textContent = 0;
 scorePlayer2El.textContent = 0;
+diceEl.classList.add("hidden");
+
+function entierAleatoire(min, max) {
+  return Math.floor(Math.random() * max) + min;
+}
+
+btnRoll.addEventListener("click", () => {
+  let randomNumber = entierAleatoire(1, 6);
+
+  diceEl.src = `img/dice-${randomNumber}.png`;
+  diceEl.classList.remove("hidden");
+
+  if (randomNumber !== 1) {
+    currentScore += randomNumber;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    sectionPlayer1.classList.toggle("player--active");
+    sectionPlayer2.classList.toggle("player--active");
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+  }
+});
